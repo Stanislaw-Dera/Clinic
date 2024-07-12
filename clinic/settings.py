251296c2d9ecd6cec4b72c37e4c0d50d28ba7b4d
Stanisław Dera <stanislaw.dera@online.lo1.info.pl>
2024-07-12 12:40:54 +0200

    Layout for welcome page.

--- clinic/settings.py
+++ clinic/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'clinic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'users/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
